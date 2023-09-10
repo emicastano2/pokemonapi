3fec9df5948317b6c9eee8cfa59ac4b9f1045d41
Emiliano Castaño <emicastano@hotmail.com>
2023-09-10 20:11:23 -0300

    Versión final

--- funciones.js
+++ funciones.js
@@ -1,22 +1,20 @@
-const listaPokemon = document.querySelectorAll("listaPokemon");
-let url = "https://pokeapi.co/api/v2/pokemon/";
+const listaPokemon = document.querySelector("#listaPokemon");
+let URL = "https://pokeapi.co/api/v2/pokemon/";
 
-for (let i = 1; i <=150, i++;) {
-    fetch(url + i)
+for (let i = 1; i <=150; i++) {
+    fetch(URL + i)
     .then((response) => response.json())
     .then(data => mostrarPokemon(data))
 }
 
-function mostrarPokemon(data) {
+function mostrarPokemon(poke){
     const div = document.createElement("div");
     div.classList.add("pokemon");
-    div.innerHTML = `
-    <div class="todos" id="listaPokemon">
-    <div class="pokemon">
-        <p class="numero">#001</p>
-        <img class="imagen" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur">
-        <h2 class="nombre">Bulbasaur</h2>
-    </div>
-    `;
-    listaPokemon.appendChild(div);
+    div.innerHTML = `<div class="pokemon">
+    <p class="numero">${poke.id}</p>
+    <img class="imagen" src="${poke.sprites.other["official-artwork"].front_default}" alt="${poke.name}">
+    <h2 class="nombre">${poke.name}</h2>
+</div>
+</div>`
+listaPokemon.append(div); 
 }
